a1fe4ea5994490857cfc564cc1f770d02d7ad8e4
Louis Collard <louiscollard2@gmail.com>
2022-08-29 15:35:12 +0200

    Add Pages and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Home } from "./Pages/Home";
+import { ProductsDetails } from "./Pages/ProductsDetails";
+import { ProductsPage } from "./Pages/ProductsPage";
+import { About } from "./Pages/About";
+import { Cart } from "./Pages/Cart";
+import { LoginPage } from "./Pages/LoginPage";
+import { RegisterPage } from "./Pages/RegisterPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Home />
+		</>
+	);
 }
 
 export default App;
