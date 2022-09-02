c4030d1da567bed959413cde52a36c87cea424d0
Llyllyra <llyllyrallylly@gmail.com>
2022-09-02 09:01:12 +0200

    draft vendredi matin

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +19,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Author::factory()->count(20)->create();
+        Book::factory()->count(20)->create();
+        Category::factory()->count(20)->create();
+        Order::factory()->count(20)->create();
+        User::factory()->count(20)->create();
     }
 }
