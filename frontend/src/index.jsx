642e07f0cc758ce46f83c0d073c02171b7885918
Louis Collard <louiscollard2@gmail.com>
2022-09-26 10:28:50 +0200

    Add stripe

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById("root"), HTMLElement);
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
